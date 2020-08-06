cb80d7a0d0a59cb09d51e5f9668f1f3a0423e05a
yemiwebby <yemiwebby@gmail.com>
2020-08-06 15:50:05 +0100

    Initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react';
+import ReactDOM from "react-dom";
+import Chat from './Chat';
+import '../css/app.css'
+
+
+ ReactDOM.render(<Chat/>, document.getElementById("root"));
